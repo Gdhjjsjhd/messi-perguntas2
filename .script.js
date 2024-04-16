ec49a32904db049db962abc52b069fb465ad729a
Gabriel <160670197+Gdhjjsjhd@users.noreply.github.com>
2024-04-16 17:12:26 -0300

    Add files via upload

---
+++ .script.js
@@ -0,0 +1,10 @@
+function messiEscolhe() {
+    var opcao1 = document.getElementById("input1").value;
+    var opcao2 = document.getElementById("input2").value;
+
+    var opcoes = [opcao1, opcao2];
+    var escolhaAleatoria = opcoes[Math.floor(Math.random() * opcoes.length)]
+    console.log(escolhaAleatoria)
+
+    document.getElementById("Resultado").innerHTML = "O Messi escolheu: " +  escolhaAleatoria;
+}
